2eb4fa157eb7148a803967387880e523a04a0d7f
Simon Conseil <contact@saimon.org>
2015-12-17 13:00:52 +0100

    Add documentation in the report

--- doit/cmd_report.py
+++ doit/cmd_report.py
@@ -25,24 +25,24 @@ class Report(DoitCmdBase):
         # dict of all tasks
         tasks = dict([(t.name, t) for t in self.task_list])
         task_list = self.task_list
-        # write = self.outstream.write
-        out = {}
+        stat = {}
 
         for task in task_list:
             if task.has_subtask:
-                out[task.name] = d = defaultdict(int)
+                stat[task.name] = d = defaultdict(int)
                 for subtask in subtasks_iter(tasks, task):
                     res = self.dep_manager.get_status(subtask, tasks)
                     d[res.status] += 1
                 d['total'] = sum(d.values())
 
         states = ['run', 'error', 'up-to-date', 'total']
-        cols = [out.keys()]
+        cols = [stat.keys()]
         for state in states:
-            cols.append([out[name][state] for name in cols[0]])
+            cols.append([stat[name][state] for name in cols[0]])
+        cols.append([tasks[name].doc for name in cols[0]])
 
-        t = Table(cols, names=['name'] + states)
-        print t
+        t = Table(cols, names=['name'] + states + ['doc'])
+        self.outstream.write(t)
 
         if html:
             t.write(html, format='jsviewer')
